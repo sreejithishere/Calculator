351fa37d90f0fb3849cc423d93c36e7bf23b5a6a
Sreejith R <sreejith.rajendran@outlook.com>
2021-10-03 20:02:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const userInput =  document.querySelector("#user_input");
+var expression = "";
+press = (num) => {
+  expression += num;
+  userInput.value = expression;
+}
+equal = () => {
+  userInput.value = eval(expression);
+  expression = "";
+}
+erase = () => {
+  expression = "";
+  userInput.value = expression;
+}
